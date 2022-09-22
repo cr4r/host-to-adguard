75d1279587725eb017976288a9d32441c9c8a10c
cr4r <cr4rrr@gmail.com>
2022-09-22 23:07:28 +0700

    add fitur oengecualian

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const cpl = require('@adguard/hostlist-compiler')
 const { writeFileSync } = require('fs')
 const config = require('./config.json')
 
+const exclusions = [
+  "googlesyndication.com"
+]
+
 const run = async (cfg) => {
   a = await cpl(cfg)
-  b = a.filter(isi => isi.length > 0).join('\n').replace('@adguard\/hostlist-compiler', config.name)
+  b = a.filter(isi => exclusions.includes(isi.replace(/[\|^]/g, '')) && isi.length > 0)
+    .join('\n')
+    .replace('@adguard\/hostlist-compiler', config.name)
   await writeFileSync('hosts', b)
 }
 
